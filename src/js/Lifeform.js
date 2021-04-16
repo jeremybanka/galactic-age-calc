d4010c9e81fbb21ab3126cb202fcae16113831dc
Jeremy Banka <hello@jeremybanka.com>
2021-04-16 12:29:21 -0700

    ✅ pass test for remaining life Lifeform method

--- src/js/Lifeform.js
+++ src/js/Lifeform.js
@@ -12,7 +12,28 @@ export default class Lifeform {
     return this.age / PLANETS[PLANET].solarLapTime
   }
 
-  cyclesYetToSeeOf(PLANET) {
-    return this.species.terranLifeExpectancy
+  cyclesYetToSeeOf(planetName) {
+    const planet = PLANETS[planetName]
+    let { average, record } = this.species.terranLifeExpectancy
+    const nativeCyclesPerTerranCycle = 1 / this.homeworld.solarLapTime
+    average *= nativeCyclesPerTerranCycle
+    record *= nativeCyclesPerTerranCycle
+    const nativeLifeExpectancy = { average, record }
+    console.log(nativeCyclesPerTerranCycle)
+    const remainingNativeLifeExpectancy = {
+      average: nativeLifeExpectancy.average - this.age,
+      record: nativeLifeExpectancy.record - this.age,
+    }
+    console.log(remainingNativeLifeExpectancy)
+    const relativeSpeed = this.homeworld.solarLapTime / planet.solarLapTime
+    console.log(this.homeworld)
+    console.log(planet)
+
+    const cyclesYet = {
+      average: remainingNativeLifeExpectancy.average * relativeSpeed,
+      record: remainingNativeLifeExpectancy.record * relativeSpeed,
+    }
+
+    return cyclesYet
   }
 }

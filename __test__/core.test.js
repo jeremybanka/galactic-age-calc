51a83ece03b6537cbddba807e352d0daa72847ec
Jeremy Banka <hello@jeremybanka.com>
2021-04-16 10:23:16 -0700

    ❓ add test for Lifeform class

--- __test__/core.test.js
+++ __test__/core.test.js
@@ -1,9 +1,21 @@
-import double from "../src/js/core"
+import { Lifeform } from "../src/js/core"
 
-describe(`double`, () => {
-  it(`doubles a number`, () => {
-    const input = 1
-    const expected = 2
-    expect(double(input)).toEqual(expected)
+describe(`new Lifeform(species, age, homeworld)`, () => {
+  it(`creates a lifeform with specified species, age, and homeworld`, () => {
+    const lifeform = new Lifeform(23, `HUMAN`, `EARTH`)
+    const expected = {
+      age: 23,
+      species: {
+        id: `HUMAN`,
+        adj: `Human`,
+        terranLifeExpectancy: [72, 122],
+      },
+      homeworld: {
+        id: `EARTH`,
+        adj: `Terran`,
+        solarLapTime: 1,
+      },
+    }
+    expect(lifeform).toEqual(expected)
   })
 })

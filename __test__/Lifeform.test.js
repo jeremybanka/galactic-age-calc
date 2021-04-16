d61f77f7f4944408c7d328d3a52066664356e96f
Jeremy Banka <hello@jeremybanka.com>
2021-04-16 13:06:40 -0700

    ❓add tests for native life expectancy methods anticipating current method refactor

--- __test__/Lifeform.test.js
+++ __test__/Lifeform.test.js
@@ -29,10 +29,34 @@ describe(`new Lifeform(age, species, homeworld)`, () => {
 })
 
 describe(`Lifeform.prototype.cyclesSeenOf(PLANET_X)`, () => {
-  it(`returns how many Sol-cycles of MARS kitty saw from EARTH`, () => {
-    const kitty = new Lifeform(1, CAT, MARS)
-    const output = kitty.cyclesSeenOf(MARS)
-    expect(output).toBe(1 / 1.88)
+  it(`returns how many Sol-cycles of MERCURY kitty saw from EARTH`, () => {
+    const kitty = new Lifeform(1, CAT, MERCURY)
+    const output = kitty.cyclesSeenOf(MERCURY)
+    expect(output).toBe(1 / 0.24)
+  })
+})
+
+describe(`Lifeform.prototype.nativeLifeExpectancy()`, () => {
+  it(`returns average lifetime venus sol-cycles for a venusian crow`, () => {
+    const venusianCrow = new Lifeform(12, BIRD, VENUS)
+    const output = venusianCrow.nativeLifeExpectancy()
+    const expected = {
+      average: (1 / 0.65) * 10,
+      record: (1 / 0.65) * 120,
+    }
+    expect(output).toEqual(expected)
+  })
+})
+
+describe(`Lifeform.prototype.nativeCyclesYetToSee()`, () => {
+  it(`returns how many mars years a martian spider might remain for`, () => {
+    const spiderFromMars = new Lifeform(0.5, SPIDER, MARS)
+    const output = spiderFromMars.nativeCyclesYetToSee()
+    const expected = {
+      average: (1 / 1.88) * 1 - 0.5,
+      record: (1 / 1.88) * 46 - 0.5,
+    }
+    expect(output).toEqual(expected)
   })
 })
 
@@ -41,8 +65,8 @@ describe(`Lifeform.prototype.cyclesYetToSeeOf(PLANET_X)`, () => {
     const me = new Lifeform(25, HUMAN, EARTH)
     const output = me.cyclesYetToSeeOf(JUPITER)
     const expected = {
-      average: (72 - 25 * 1) / 11.86,
-      record: (122 - 25 * 1) / 11.86,
+      average: 72 / 11.86 - 25 / 11.86,
+      record: 122 / 11.86 - 25 / 11.86,
     }
     expect(output).toEqual(expected)
   })

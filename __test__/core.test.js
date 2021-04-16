042f4a5a1fe5946a81fe097c8594632214b1ea62
Jeremy Banka <hello@jeremybanka.com>
2021-04-16 10:46:10 -0700

    ❓test for cycles seen of foreign world

--- __test__/core.test.js
+++ __test__/core.test.js
@@ -1,4 +1,7 @@
 import { Lifeform } from "../src/js/core"
+import PLANETS from "../src/js/data/planetarium"
+
+const [MERCURY, VENUS, EARTH, MARS, JUPITER] = Object.keys(PLANETS)
 
 describe(`new Lifeform(species, age, homeworld)`, () => {
   it(`creates a lifeform with specified species, age, and homeworld`, () => {
@@ -17,5 +20,13 @@ describe(`new Lifeform(species, age, homeworld)`, () => {
       },
     }
     expect(lifeform).toEqual(expected)
+  })
+})
+
+describe(`Lifeform.prototype.cyclesSeenOf(PLANET_X)`, () => {
+  it(`returns how many Sol-cycles of MARS kitty saw from EARTH`, () => {
+    const kitty = new Lifeform(1, `CAT`, `EARTH`)
+    const output = kitty.cyclesSeenOf(MARS)
+    expect(output).toBe(1.88)
   })
 })

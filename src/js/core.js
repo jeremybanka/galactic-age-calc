0597a22c578b7180dd53b5093454734b001e972e
Jeremy Banka <hello@jeremybanka.com>
2021-04-18 12:40:58 -0700

    ✅ pass tests for ordinal maker

--- src/js/core.js
+++ src/js/core.js
@@ -1,3 +1,29 @@
+function simpleOrdinal(maybeNaturalNumber) {
+  if (maybeNaturalNumber < 0 || !Number.isInteger(maybeNaturalNumber)) {
+    throw new Error(`${maybeNaturalNumber} is not a natural number`)
+  }
+  let ordinalSuffix = `th`
+  const numString = `${maybeNaturalNumber}`
+  const onesAndTens = numString.slice(-2)
+  const numberIsATween = [`11`, `12`, `13`].includes(onesAndTens)
+  if (!numberIsATween) {
+    const onesPlace = numString.slice(-1)
+    ordinalSuffix = (() => {
+      switch (onesPlace) {
+        case `1`:
+          return `st`
+        case `2`:
+          return `nd`
+        case `3`:
+          return `rd`
+        default:
+          return `th`
+      }
+    })()
+  }
+  return numString + ordinalSuffix
+}
+
 function pickRandom(array) {
   const randomIdx = d(array.length) - 1
   return array[randomIdx]
@@ -14,4 +40,4 @@ function d(howManyFaces = 2, howManyDice = 1) {
   return gross
 }
 
-export { pickRandom, d }
+export { simpleOrdinal, pickRandom, d }

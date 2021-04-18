35ee9f77985549dbb9663c7c71ce10807be20870
Jeremy Banka <hello@jeremybanka.com>
2021-04-18 12:40:18 -0700

    ❓add test for ordinal maker

--- __test__/core.test.js
+++ __test__/core.test.js
@@ -1,4 +1,28 @@
-import { pickRandom, d } from "../src/js/core"
+import { pickRandom, d, simpleOrdinal } from "../src/js/core"
+
+describe(`simpleOrdinal(maybeNaturalNumber)`, () => {
+  it(`gives '1st' for 1`, () => {
+    expect(simpleOrdinal(1)).toBe(`1st`)
+  })
+  it(`gives '32nd' for 32`, () => {
+    expect(simpleOrdinal(32)).toBe(`32nd`)
+  })
+  it(`gives '333rd' for 333`, () => {
+    expect(simpleOrdinal(333)).toBe(`333rd`)
+  })
+  it(`gives '177th' for 177`, () => {
+    expect(simpleOrdinal(177)).toBe(`177th`)
+  })
+  it(`gives '11th' for 11`, () => {
+    expect(simpleOrdinal(11)).toBe(`11th`)
+  })
+  it(`gives '12th' for 12`, () => {
+    expect(simpleOrdinal(12)).toBe(`12th`)
+  })
+  it(`gives '13th' for 13`, () => {
+    expect(simpleOrdinal(13)).toBe(`13th`)
+  })
+})
 
 describe(`pickRandom(array)`, () => {
   it(`rolls a die with 6 faces and returns the result`, () => {
